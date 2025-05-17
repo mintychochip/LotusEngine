3668abc88efdfc8512e8f2b2089adffc8231f9e9
justi <jlo2@csub.edu>
2025-05-17 01:40:36 -0700

    removed build

--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/justi/dev/engine/googletest/googlemock
-# Build directory: C:/Users/justi/dev/engine/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
