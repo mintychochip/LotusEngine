b08e51ee5e5b324e87a93ecf63449fc6248af064
justi <jlo2@csub.edu>
2025-05-19 00:47:22 -0700

    fixed submodule changed directory structure

--- core/containers.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include "allocator.hpp"
-using u32 = uint32_t;
-struct FreeBlock
-{
-    FreeBlock *next;
-};
--- core/utils.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-using u32 = uint32_t;
-constexpr u32 KB(u32 x) { return x * 1024; }
-constexpr u32 MB(u32 x) { return x * 1024 * 1024; }
